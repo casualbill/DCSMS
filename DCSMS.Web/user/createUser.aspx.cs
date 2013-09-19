f8cee6dcaab008cfae92f8e96a3be4286c2c7b8c
casualbill <hotwindzb@gmail.com>
2013-09-20 00:53:21 +0800

    Create user

---
+++ DCSMS.Web/user/createUser.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DCSMS.BLL;
+
+namespace DCSMS.Web.user
+{
+    public partial class createUser : System.Web.UI.Page
+    {
+        protected UserLogic userLogic = new UserLogic();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            String userName = tb_username.Text.Trim().ToLower();
+            String password = tb_password.Text;
+            String passwordRepeat = tb_password_repeat.Text;
+
+            if (userName.Length < 4 || password.Length < 6 || password != passwordRepeat)
+            {
+                lb_tips.Text = "error";
+            }
+            else
+            {
+                int retVal = userLogic.createUser(userName, password, 1);
+                if (retVal == 1)
+                {
+                    lb_tips.Text = "success";
+                }
+                else if (retVal == -1)
+                {
+                    lb_tips.Text = "user exist";
+                }
+                else
+                {
+                    lb_tips.Text = "fail";
+                }
+            }
+        }
+    }
+}
