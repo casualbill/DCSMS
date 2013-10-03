74142819236693288d2034664db42fdc9fedfdb6
casualbill <hotwindzb@gmail.com>
2013-10-03 21:08:12 +0800

    Test

--- DCSMS.Web/test.aspx.cs
+++ DCSMS.Web/test.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DCSMS.BLL;
+using System.Collections.Generic;
 
 namespace DCSMS.Web
 {
@@ -13,11 +14,48 @@ namespace DCSMS.Web
             //dataGrid.DataSource = testLogic.testQueryList();
             //dataGrid.DataBind();
             //label.Text = testLogic.testQueryCount();
+
+            Object[] objArray = new Object[5] { "a", 1, 5, "b", "x" };
+            objTest(objArray);
+
+            List<List<String>> listArray = new List<List<String>>();
+
+            for (int i = 1; i < 5; i++) {
+                List<String> subList = new List<string>();
+                for (int j = 1; j < 7; j++)
+                {
+                    subList.Add((i * 10 + j).ToString());
+                }
+                listArray.Add(subList);
+            }
+
+            doubleListTest(listArray);
+            
         }
 
         protected void onButtonClick(object sender, EventArgs e)
         {
             label.Text = userLogic.getMD5HashCode(textBox.Text);
+        }
+
+        protected void objTest(Object[] objA)
+        {
+            foreach (Object obj in objA)
+            {
+                Response.Write(obj.ToString());
+            }
+        }
+
+        protected void doubleListTest(List<List<String>> listArray)
+        {
+            foreach (List<String> subList in listArray)
+            {
+                foreach (String str in subList)
+                {
+                    Response.Write(str + " ");
+                }
+                Response.Write("<br />");
+            }
         }
     }
 }
