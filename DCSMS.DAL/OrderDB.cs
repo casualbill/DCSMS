8fa4692329cfbaf25099beea7dbbdf9d24f4d270
casualbill <hotwindzb@gmail.com>
2013-10-03 15:10:20 +0800

    Order info in order DB

--- DCSMS.DAL/OrderDB.cs
+++ DCSMS.DAL/OrderDB.cs
@@ -9,7 +9,40 @@ namespace DCSMS.DAL
 {
     public class OrderDB : DBHelper
     {
-        public int orderOperationInsert(String orderId, int userId, int formerStatus, int newStatus, DateTime operateTime)
+        public int orderCreate(String id, String failureDescription, String imgUrl, DateTime createTime, int stationId)
+        {
+            String sqlCommand = "insert into orderinfo values (@Id, @FailureDescription, @ImgUrl, @CreateTime, null, @StationId, 1)";
+
+            List<MySqlParameter> paramList = new List<MySqlParameter>();
+            paramList.Add(new MySqlParameter("@Id", id));
+            paramList.Add(new MySqlParameter("@FailureDescription", failureDescription));
+            paramList.Add(new MySqlParameter("@ImgUrl", imgUrl));
+            paramList.Add(new MySqlParameter("@CreateTime", createTime));
+            paramList.Add(new MySqlParameter("@StationId", stationId));
+
+            return executeSqlCommandNoQuery(sqlCommand, paramList);
+        }
+
+        public int orderStatusUpdate(int orderStatus, DateTime updateTime, String id)
+        {
+            String sqlCommand = "update orderinfo set OrderStatus = @OrderStatus, UpdateTime = @UpdateTime where Id = @Id";
+
+            List<MySqlParameter> paramList = new List<MySqlParameter>();
+            paramList.Add(new MySqlParameter("@OrderStatus", orderStatus));
+            paramList.Add(new MySqlParameter("@UpdateTime", updateTime));
+            paramList.Add(new MySqlParameter("@Id", id));
+
+            return executeSqlCommandNoQuery(sqlCommand, paramList);
+        }
+
+        public DataSet orderQuery(String id)
+        {
+            String sqlCommand = "select * from orderinfo where Id = @Id";
+            MySqlParameter param = new MySqlParameter("@Id", id);
+            return executeSqlCommandDataSet(sqlCommand, param);
+        }
+
+        public int orderLogInsert(String orderId, int userId, int formerStatus, int newStatus, DateTime operateTime)
         {
             String sqlCommand = "insert into orderlog values (null, @OrderId, @UserId, @FormerStatus, @NewStatus, @OperateTime)";
 
@@ -23,7 +56,7 @@ namespace DCSMS.DAL
             return executeSqlCommandNoQuery(sqlCommand, paramList);
         }
 
-        public DataSet orderOperationQuery(String orderId)
+        public DataSet orderLogQuery(String orderId)
         {
             String sqlCommand = "select * from orderlog where orderId = @OrderId";
             MySqlParameter param = new MySqlParameter("@OrderId", orderId);
