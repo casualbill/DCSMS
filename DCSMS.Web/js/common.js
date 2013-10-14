4f4940ff0bbff9ed85efc5e0a92eee71e7e0e292
casualbill <hotwindzb@gmail.com>
2013-10-14 15:32:30 +0800

    Main menu mouse hover effect

---
+++ DCSMS.Web/js/common.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('.header-menu dl').on('mouseenter', function () {
+        $(this).children('dt').css({ 'border-left': '1px solid #FFF', 'border-right': '1px solid #FFF', 'border-top': '1px solid #FFF' });
+        $(this).children('dd').show();
+    });
+
+    $('.header-menu dl').on('mouseleave', function () {
+        $(this).children('dt').css({ 'border-left': '1px solid #000', 'border-right': '1px solid #000', 'border-top': '1px solid #000' });
+        $(this).children('dd').hide();
+    });
+});
