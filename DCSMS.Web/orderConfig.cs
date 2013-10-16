a9061282a89c962e86ab4410367fba08e20c6e5b
casualbill <hotwindzb@gmail.com>
2013-10-16 16:30:22 +0800

    Remove codes for user & customer drop down list

--- DCSMS.Web/orderConfig.cs
+++ DCSMS.Web/orderConfig.cs
@@ -10,33 +10,33 @@ namespace DCSMS.Web
 {
     public class orderConfig
     {
-        public void loadCustomerList(DropDownList ddl_customer, Boolean IsOnlyVerified, Boolean addDefaultItem)
-        {
-            CustomerLogic customerLogic = new CustomerLogic();
-            DataTable dt;
-            if (IsOnlyVerified == true)
-            {
-                dt=customerLogic.verifiedCustomerQuery();
-            }
-            else
-            {
-                dt = customerLogic.customerQuery();
-            }
+        //public void loadCustomerList(DropDownList ddl_customer, Boolean IsOnlyVerified, Boolean addDefaultItem)
+        //{
+        //    CustomerLogic customerLogic = new CustomerLogic();
+        //    DataTable dt;
+        //    if (IsOnlyVerified == true)
+        //    {
+        //        dt=customerLogic.verifiedCustomerQuery();
+        //    }
+        //    else
+        //    {
+        //        dt = customerLogic.customerQuery();
+        //    }
 
-            ddl_customer.Items.Clear();
+        //    ddl_customer.Items.Clear();
 
-            if (addDefaultItem == true)
-            {
-                ddl_customer.Items.Add(new ListItem("请选择客户", "0"));
-            }
-            if (dt != null)
-            {
-                foreach (DataRow dr in dt.Rows)
-                {
-                    ddl_customer.Items.Add(new ListItem(dr["CustomerName"].ToString(), dr["Id"].ToString()));
-                }
-            }
-        }
+        //    if (addDefaultItem == true)
+        //    {
+        //        ddl_customer.Items.Add(new ListItem("请选择客户", "0"));
+        //    }
+        //    if (dt != null)
+        //    {
+        //        foreach (DataRow dr in dt.Rows)
+        //        {
+        //            ddl_customer.Items.Add(new ListItem(dr["CustomerName"].ToString(), dr["Id"].ToString()));
+        //        }
+        //    }
+        //}
 
         public void loadStationList(DropDownList ddl_station)
         {
@@ -54,33 +54,33 @@ namespace DCSMS.Web
             }
         }
 
-        public void loadUserList(DropDownList ddl_task_user, Boolean isTechnician, Boolean addDefaultItem)
-        {
-            UserLogic userLogic = new UserLogic();
-            DataTable dt;
-            if (isTechnician == true)
-            {
-                dt = userLogic.userQueryByUserType(2);
-            }
-            else
-            {
-                dt = userLogic.userQuery();
-            }
+        //public void loadUserList(DropDownList ddl_task_user, Boolean isTechnician, Boolean addDefaultItem)
+        //{
+        //    UserLogic userLogic = new UserLogic();
+        //    DataTable dt;
+        //    if (isTechnician == true)
+        //    {
+        //        dt = userLogic.userQueryByUserType(2);
+        //    }
+        //    else
+        //    {
+        //        dt = userLogic.userQuery();
+        //    }
 
-            ddl_task_user.Items.Clear();
+        //    ddl_task_user.Items.Clear();
 
-            if (addDefaultItem == true)
-            {
-                ddl_task_user.Items.Add(new ListItem("请选择用户", "0"));
-            }
+        //    if (addDefaultItem == true)
+        //    {
+        //        ddl_task_user.Items.Add(new ListItem("请选择用户", "0"));
+        //    }
 
-            if (dt != null)
-            {
-                foreach (DataRow dr in dt.Rows)
-                {
-                    ddl_task_user.Items.Add(new ListItem(dr["UserName"].ToString(), dr["Id"].ToString()));
-                }
-            }
-        }
+        //    if (dt != null)
+        //    {
+        //        foreach (DataRow dr in dt.Rows)
+        //        {
+        //            ddl_task_user.Items.Add(new ListItem(dr["UserName"].ToString(), dr["Id"].ToString()));
+        //        }
+        //    }
+        //}
     }
 }
