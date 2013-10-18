23d937c27543ee4719feca9036d6571fc18eac73
casualbill <hotwindzb@gmail.com>
2013-10-18 15:55:05 +0800

    Error handler

---
+++ DCSMS.BLL/ErrorHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace DCSMS.BLL
+{
+    public class ErrorHandler : System.Web.UI.Page
+    {
+        public static String logFile;
+
+        public void WriteLog(string logStr)
+        {
+            StreamWriter streamWriter = null;
+            logFile = Server.MapPath("/") + "\\errorlog\\log" + System.DateTime.Now.Date.ToString("yyMMdd") + ".txt";
+
+            try
+            {
+                FileInfo finfo = new FileInfo(logFile);
+                if (finfo.Exists && finfo.Length > 10485760)
+                {
+                    finfo.Delete();
+                }
+                streamWriter = new StreamWriter(@logFile, true, System.Text.UnicodeEncoding.Unicode);
+                streamWriter.WriteLine(System.DateTime.Now.ToString() + "   " + logStr);
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                throw (new Exception(uae.Message + logFile));
+            }
+            catch (DirectoryNotFoundException de)
+            {
+                throw (de);
+            }
+            catch (System.Security.SecurityException se)
+            {
+                throw (se);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            finally
+            {
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+        }
+    }
+}
---
+++ DCSMS.Web/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using DCSMS.BLL;
+
+namespace DCSMS.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception lastError = Server.GetLastError();
+            String errMessage = lastError.ToString();
+            String msgStr = "Url：" + Request.Path + " Error: " + errMessage;
+
+            ErrorHandler errorHandler = new ErrorHandler();
+            errorHandler.WriteLog(msgStr);
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
