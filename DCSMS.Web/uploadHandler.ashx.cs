059930b63f8dceb4c2d9a1ca3d417df34037a30d
casualbill <hotwindzb@gmail.com>
2013-10-21 14:37:26 +0800

    Upload handler

---
+++ DCSMS.Web/uploadHandler.ashx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace DCSMS.Web
+{
+    public class uploadHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Charset = "utf-8";
+
+            HttpPostedFile file = context.Request.Files["Filedata"];
+            String uploadPath = HttpContext.Current.Server.MapPath("uploads") + "\\";
+
+            if (file != null)
+            {
+                if (!Directory.Exists(uploadPath))
+                {
+                    Directory.CreateDirectory(uploadPath);
+                }
+                file.SaveAs(uploadPath + file.FileName);
+
+                context.Response.Write("1");
+            }
+            else
+            {
+                context.Response.Write("0");
+            }  
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
