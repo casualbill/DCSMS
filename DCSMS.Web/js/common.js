24dc4c4db91de7fdccd5a1c8fd54c34b54827892
casualbill <hotwindzb@gmail.com>
2013-10-23 14:26:35 +0800

    Listen main menu list hover event only in masterpage

--- DCSMS.Web/js/common.js
+++ DCSMS.Web/js/common.js
@@ -1,4 +1,4 @@
-﻿$(function () {
+﻿var mainMenuListHover = function () {
     $('.header-menu dl').on('mouseenter', function () {
         $(this).children('dt').css({ 'border-left': '1px solid #FFF', 'border-right': '1px solid #FFF', 'border-top': '1px solid #FFF' });
         $(this).children('dd').show();
@@ -8,7 +8,7 @@
         $(this).children('dt').css({ 'border-left': '1px solid #000', 'border-right': '1px solid #000', 'border-top': '1px solid #000' });
         $(this).children('dd').hide();
     });
-});
+}
 
 var getQueryStringByName = function (name) {
     var result = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
