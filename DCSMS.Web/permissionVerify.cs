ea2410b3dfe3de7c6645642230d5e573221bc942
casualbill <hotwindzb@gmail.com>
2013-10-24 15:56:40 +0800

    Permission verify method

---
+++ DCSMS.Web/permissionVerify.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DCSMS.Web
+{
+    public class permissionVerify : System.Web.UI.Page
+    {
+        public void permissionVerify(int allowUserType)
+        {
+            if (Session["userid"] == null)
+            {
+                Response.Write("<script type=\"text/javascript\">alert (\"登录已超时，请重新登录！\"); window.location.href=\"/login.aspx\";</script>");
+                return;
+            }
+
+            int userType = Convert.ToInt16(Session["userType"]);
+
+            if (userType < allowUserType)
+            {
+                Response.Redirect("/error.aspx");
+            }
+        }
+    }
+}
