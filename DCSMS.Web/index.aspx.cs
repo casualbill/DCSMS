036d7cee40c605161fa34e6dd4742ed92b66ad91
casualbill <hotwindzb@gmail.com>
2013-10-25 17:30:47 +0800

    Create index page

---
+++ DCSMS.Web/index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DCSMS.Web
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Convert.ToInt16(Session["userType"]) == 1)
+            {
+                Response.Redirect("/order/orderQuery.aspx");
+            }
+            else
+            {
+                Response.Redirect("/myTask.aspx");
+            }
+        }
+    }
+}
