82bc18303a6140066a2a777365f73ac08d994d04
casualbill <hotwindzb@gmail.com>
2013-11-04 17:59:11 +0800

    Region DB

---
+++ DCSMS.DAL/RegionDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DCSMS.DAL
+{
+    public class RegionDB : DBHelper
+    {
+        public DataSet provinceListQuery()
+        {
+            String sqlCommand = "select * from provincelist";
+            return executeSqlCommandDataSet(sqlCommand);
+        }
+
+        public DataSet cityListQuery(int provinceId)
+        {
+            String sqlCommand = "select * from cityinfo where ProvinceId = @ProvinceId";
+            MySqlParameter param = new MySqlParameter("@ProvinceId", provinceId);
+            return executeSqlCommandDataSet(sqlCommand, param);
+        }
+    }
+}
