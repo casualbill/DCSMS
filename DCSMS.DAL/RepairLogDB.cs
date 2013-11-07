9d9d796fe1f80c613cc5bcb70bef3787bd8b4cce
casualbill <hotwindzb@gmail.com>
2013-11-07 11:20:54 +0800

    Adjust repair log DB

--- DCSMS.DAL/RepairLogDB.cs
+++ DCSMS.DAL/RepairLogDB.cs
@@ -7,27 +7,31 @@ namespace DCSMS.DAL
 {
     public class RepairLogDB : DBHelper
     {
-        public int repairLogCreate(DateTime startTime, DateTime endTime, String workDetail, String orderId)
+        public int repairLogCreate(DateTime startTime, DateTime endTime, String workDetail, String defaultCharacter, String workTime, String orderId)
         {
-            String sqlCommand = "insert into repairlog values (null, @StartTime, @EndTime, @WorkDetail, @OrderId)";
+            String sqlCommand = "insert into repairlog values (null, @StartTime, @EndTime, @WorkDetail, @DefaultCharacter, @WorkTime, @OrderId)";
 
             List<MySqlParameter> paramList = new List<MySqlParameter>();
             paramList.Add(new MySqlParameter("@StartTime", startTime));
             paramList.Add(new MySqlParameter("@EndTime", endTime));
             paramList.Add(new MySqlParameter("@WorkDetail", workDetail));
+            paramList.Add(new MySqlParameter("@DefaultCharacter", defaultCharacter));
+            paramList.Add(new MySqlParameter("@WorkTime", workTime));
             paramList.Add(new MySqlParameter("@OrderId", orderId));
 
             return executeSqlCommandNoQuery(sqlCommand, paramList);
         }
 
-        public int repairLogUpdate(DateTime startTime, DateTime endTime, String workDetail, int id)
+        public int repairLogUpdate(DateTime startTime, DateTime endTime, String workDetail, String defaultCharacter, String workTime, int id)
         {
-            String sqlCommand = "update repairlog set StartTime = @StartTime, EndTime = @EndTime, WorkDetail = @WorkDetail where Id = @Id";
+            String sqlCommand = "update repairlog set StartTime = @StartTime, EndTime = @EndTime, WorkDetail = @WorkDetail, DefaultCharacter = @DefaultCharacter, WorkTime = @WorkTime where Id = @Id";
 
             List<MySqlParameter> paramList = new List<MySqlParameter>();
             paramList.Add(new MySqlParameter("@StartTime", startTime));
             paramList.Add(new MySqlParameter("@EndTime", endTime));
             paramList.Add(new MySqlParameter("@WorkDetail", workDetail));
+            paramList.Add(new MySqlParameter("@DefaultCharacter", defaultCharacter));
+            paramList.Add(new MySqlParameter("@WorkTime", workTime));
             paramList.Add(new MySqlParameter("@Id", id));
 
             return executeSqlCommandNoQuery(sqlCommand, paramList);
